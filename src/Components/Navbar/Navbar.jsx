10bf77936fbbaca96fd6c6655d26f43fed4ffb8d
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-03-12 21:40:27 +0200

    Updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Navbar.css'
+import logo_svg from '../../assets/logo_dark.svg'
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
+        <div className="imag-nav">
+            <img src={logo_svg} alt="" />
+        </div>
+
       
     </div>
   )
