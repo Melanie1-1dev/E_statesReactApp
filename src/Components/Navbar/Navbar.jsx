42087309bbf93b651a141cf6ddd19efa57bbb982
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-03-13 07:05:19 +0200

    Updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import './Navbar.css'
 import logo_svg from '../../assets/logo_dark.svg'
+import header from '../../assets/header_img.png'
 const Navbar = () => {
   return (
     <div className='navbar'>
+        <nav>
         <div className="imag-nav">
             <img src={logo_svg} alt="" />
+            
+        </div>
+
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Testimonials</a></li>
+            <li className='sign-btn'><a href="#">Sign Up</a></li>
+
+        </ul>
+        </nav>
+
+        <div className="header-text">
+            <h1>Explore homes that <br />
+            fit your dreams.
+             </h1>
+
+             <button>Projects</button>
+             <button>Contact Us</button>
         </div>
 
       
