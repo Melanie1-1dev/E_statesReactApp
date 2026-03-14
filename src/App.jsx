1a859292c7d9bf3e87a2cc17709b8e67710afbfc
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-03-14 17:16:37 +0200

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Section2 from './Components/Section2/Section2'
+import Projects from './Components/Projects/Projects'
 const App = () => {
   return (
     <div>
       <Navbar />
       <div className="container">
       <Section2 />
+      <Projects />
 
       </div>
     </div>
