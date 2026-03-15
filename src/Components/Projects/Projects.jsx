6c134dc2df0324c5714e775932f404efceee692c
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-03-15 16:19:02 +0200

    Updated

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,32 +1,23 @@
 import React from 'react'
-import './Projects.css'
-import project_1 from '../../assets/project_img_1.jpg'
-import project_2 from '../../assets/project_img_2.jpg'
-import project_3 from '../../assets/project_img_3.jpg'
-import project_4 from '../../assets/project_img_4.jpg'
-import project_5 from '../../assets/project_img_5.jpg'
-import project_6 from '../../assets/project_img_6.jpg'
+import Data from "../../Data/Data"
+import ProjectsCard from "./Projects";
 
+import './Projects.css'
 
 const Projects = () => {
   return (
-    <div className='projects'>
+    <section className='projects'>
       <div className="container">
         <h1>Projects Completed</h1>
-
-        <p>crfafting spaces, BUilding legacles-Explore <br /> Our Portfolio</p>
+        <p>Crafting spaces, Building legacles-Explore <br /> Our Portfolio</p>
       </div>
-      <div className="images">
-        <img src={project_1} alt="" />
-        <img src={project_2} alt="" />
-        <img src={project_3} alt="" />
-        <img src={project_4} alt="" />
-        <img src={project_5} alt="" />
-        <img src={project_6} alt="" />
-      </div>
+      <div className="projects-grid">
+        {Data.map((Data, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
+      </div>   
       
-      
-    </div>
+    </section>
   )
 }
 
