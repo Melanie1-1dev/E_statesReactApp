6ed5aec237f1aa3b667fcd5ebbd423e98f8cf801
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-03-17 21:02:10 +0200

    Updated

---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <div className='testimonials'>
+        <div className="container">
+            <h1>Customer <span>Testimonials</span></h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Testimonials
