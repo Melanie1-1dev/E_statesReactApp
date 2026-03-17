b0d73b025295348c8a0534f0adabc96982a82160
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-03-17 21:09:30 +0200

    Updated

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -6,7 +6,11 @@ const Testimonials = () => {
     <div className='testimonials'>
         <div className="container">
             <h1>Customer <span>Testimonials</span></h1>
+
+            <p>Real stories from Those Who Found Home <br />with Us</p>
+            
         </div>
+
       
     </div>
   )
