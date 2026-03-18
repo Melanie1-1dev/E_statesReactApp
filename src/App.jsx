2001c299fcfdc7815f34b7abdd8ae7fe32aa4b3a
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-03-18 09:53:33 +0200

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Section2 from './Components/Section2/Section2'
 import Projects from './Components/Projects/Projects'
+import Testimonials from './Components/Testimonials/Testimonials'
 const App = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const App = () => {
       <div className="container">
       <Section2 />
       <Projects />
+      <Testimonials />
 
       </div>
     </div>
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Testimonials.css'
+import profile_1 from '../../assets/profile_img_1.png';
 
 const Testimonials = () => {
   return (
@@ -8,6 +9,39 @@ const Testimonials = () => {
             <h1>Customer <span>Testimonials</span></h1>
 
             <p>Real stories from Those Who Found Home <br />with Us</p>
+
+            <div className="images">
+              <img src={profile_1} alt="" />
+              <h3>Donald Jackman</h3>
+              <span>Marketing Manager</span>
+              <p>Fromthe very first meeting they understand my 
+                vision and helped me find the perfect property.
+                Their attention to detail and commitment to 
+                client satisfication is unmatched.
+              </p>
+            </div>
+
+            <div className="images">
+              <img src={profile_2} alt="" />
+              <h3>Richard Nelson</h3>
+              <span>Marketing Manager</span>
+              <p>Fromthe very first meeting they understand my 
+                vision and helped me find the perfect property.
+                Their attention to detail and commitment to 
+                client satisfication is unmatched.
+              </p>
+            </div>
+
+            <div className="images">
+              <img src={profile_3} alt="" />
+              <h3>Donald Jackman</h3>
+              <span>Marketing Manager</span>
+              <p>Fromthe very first meeting they understand my 
+                vision and helped me find the perfect property.
+                Their attention to detail and commitment to 
+                client satisfication is unmatched.
+              </p>
+            </div>
             
         </div>
 
